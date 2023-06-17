f02072a12df728c3dcf150efea91e80530813327
Mostafa Shawki <mostafashawki@gmail.com>
2023-06-17 22:22:13 +0200

    put data in separate json and use useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,9 @@
+import { useMemo } from "react";
 import DataTable from "./components/DataTable";
+import DATA from "./components/data.json"
 
 const App = () => {
-  const data = [
-    {
-      month: '2020-01',
-      total_value: '100000',
-      value_details_by_brand: [
-        {
-          BMW: 100000,
-        },
-        {
-          Suzuki: 25000,
-        },
-        {
-          Volvo: 25000,
-        },
-      ],
-      orders_details: [
-        {
-          order_id: '1234',
-          quantity: '1',
-          unit_price: '10000',
-          total_amount: '10000',
-        },
-        {
-          order_id: '1235',
-          quantity: '1',
-          unit_price: '10000',
-          total_amount: '10000',
-        },
-      ],
-    },
-  ];
+  const data = useMemo(() => DATA, []);
 
   return (
     <div>
